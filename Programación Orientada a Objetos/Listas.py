086ad97ad90a600fc171b5e343da5d66f6533db7
itzelrincon <72486961+itzelrincon@users.noreply.github.com>
2021-06-06 21:48:47 -0500

    Create Listas.py

---
+++ Programación Orientada a Objetos/Listas.py
@@ -0,0 +1,28 @@
+a = [0,1,2,3,4]
+a.append(5)
+print(a)
+
+a.pop(5)
+print(a)
+
+a.insert(0,5.5)
+print(a)
+
+a.pop(0)
+print(a)
+
+a.reverse()
+print(a)
+
+a.sort()
+print(a)
+
+print(a.count(2))
+
+print(len(a))
+
+print(a[3])
+
+print(a[1:5])
+
+print(2 in a)
